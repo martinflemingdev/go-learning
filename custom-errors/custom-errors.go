96d36cda03585c56ecc65d96e672523719a2d6e3
Martin Fleming <martinfleming@pop-os.localdomain>
2024-01-11 17:18:56 -0500

    adding custom-errors

---
+++ custom-errors/custom-errors.go
@@ -0,0 +1,66 @@
+package customerrors
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+type FodderCalculator interface {
+	FodderAmount(int) (float64, error)
+	FatteningFactor() (float64, error)
+}
+
+func DivideFood(fc FodderCalculator, cows int) (float64, error) {
+	totalFodder, err := fc.FodderAmount(cows)
+	if err != nil {
+		return 0, err
+	}
+	factor, err := fc.FatteningFactor()
+	if err != nil {
+		return 0, err
+	}
+	return (totalFodder / float64(cows)) * factor, nil
+}
+
+func ValidateInputAndDivideFood(fc FodderCalculator, cows int) (float64, error) {
+	if cows > 0 {
+		return DivideFood(fc, cows)
+	}
+	return 0, errors.New("invalid number of cows")
+}
+
+// define the custom error
+type InvalidCowsError struct {
+	cows    int
+	message string
+}
+
+func (e *InvalidCowsError) Error() string {
+	return fmt.Sprintf("%s cows are invalid: %s", strconv.Itoa(e.cows), e.message)
+}
+
+func ValidateNumberOfCows(cows int) error {
+	if cows < 0 {
+		return &InvalidCowsError{
+			cows:    cows,
+			message: "there are no negative cows",
+		}
+	}
+	if cows == 0 {
+		return &InvalidCowsError{
+			cows:    cows,
+			message: "no cows don't need food",
+		}
+	}
+	return nil
+}
+
+// Your first steps could be to read through the tasks, and create
+// these functions with their correct parameter lists and return types.
+// The function body only needs to contain `panic("")`.
+//
+// This will make the tests compile, but they will fail.
+// You can then implement the function logic one by one and see
+// an increasing number of tests passing as you implement more
+// functionality.
